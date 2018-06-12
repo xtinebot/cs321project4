1291593efd993f7357b42423ef627dc9efb90341
xtinebot <christinechagil@boisestate.edu>
2018-06-12 09:16:14 -0600

    initializing github project

---
+++ GeneBankCreateBTree.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+/**
+ * Driver class for creating BTree from GBK file
+ * 
+ * @author Christine Chang, Keaton Gillian, Koutam Haidara
+ *
+ */
+public class GeneBankCreateBTree {
+
+	boolean hasCache;
+	int cacheSize;
+	int degree;
+	String fileName;
+	File GBKFile;
+	int seqLength;
+	int debugLevel;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
